37a64dfe3f8a6f5fce89759d0ef5ba1045b45e35
RedTTG <redtonehair@gmail.com>
2022-12-12 00:47:47 +0200

    Create grid.py
    that has to be the most PEP certified piece of code I have ever written XD, PROVE ME WRONG LMAO

---
+++ grid.py
@@ -0,0 +1,23 @@
+class Grid:
+    width: int
+    height: int
+    separation: int
+    size: tuple[int, int]
+
+    def __init__(self, width: int, height: int, separation: int):
+        self.width = width
+        self.height = height
+        self.separation = separation
+        self.size = self.get_size()
+
+    def get_size(self) -> tuple[int, int]:
+        return (
+            self.width - 1 * self.separation,
+            self.height - 1 * self.separation
+        )
+
+    def get_point(self, x: int, y: int) -> tuple[int, int]:
+        return (
+            x * self.separation,
+            y * self.separation
+        )
